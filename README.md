ceae1fda4eac5ebc8d4c40cb85b5df337e34333e
Hugo Villevieille <hugo.villevieille@epitech.eu>
2019-08-03 03:03:45 +0200

    v0.1

---
+++ README.md
@@ -0,0 +1,25 @@
+# NextJS Boilerplate
+
+I use this boilerplate as a starting point for all my React projects, feel free to use it if you find it convenient/useful.
+
+This config supports the SSR feature provided by NextJS.
+
+## Configuration List
+
+- Basic NextJS installation
+- Semantic UI custom source (run `yarn build-semantic` to build a bundle into `/static`)
+- Styled Components
+- Airbnb eslint config
+
+## Install
+
+```
+$ yarn
+$ yarn build-semantic
+```
+
+## Usage
+
+```
+$ yarn dev
+```
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-const Home = () => {
-  return <div>Welcome to Next.js</div>;
-};
+import React from 'react';
+import styled from 'styled-components';
+import { Grid, Message } from 'semantic-ui-react';
+
+const HomeWrapper = styled.div`
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Home = () => (
+    <HomeWrapper>
+        <Grid>
+            <Grid.Row>
+                <Grid.Column>
+                    <Message>This is a NextJS Boilerplate</Message>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    </HomeWrapper>
+);
 
 export default Home;
